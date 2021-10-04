4ce8343dd3592a0ee86d4c1f8c477d0571f23686
SHeep <gabrielarruda08@gmail.com>
2021-10-04 10:35:04 -0300

    change dabase to mongo and config access

--- config/database.js
+++ config/database.js
@@ -4,8 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI', 'localhost'),
-      }
+        uri: env('DATABASE_URI'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
-});
+})
